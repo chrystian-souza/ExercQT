1e6e50f1084ea31cc3971d67e6765ba3af275849
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-03-21 20:33:46 -0300

    começou
    aula do sia 21/03/2023

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from PySide6 import *
+import sys
+from PySide6.QtWidgets import QMainWindow, QApplication
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+app = QApplication(sys.argv)
+principal = MainWindow()
+principal.show()
+app.exec()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
