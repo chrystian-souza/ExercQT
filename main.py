fd1b4ed7e4833d616aa6983e7612c9ee7abd39e7
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-03-21 21:11:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import sys
-from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QLineEdit, QPushButton, QVBoxLayout
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle('Cálculo de dentro cúbico')
+
+        self.lbl_altura = QLabel('Altura')
+        self.txt_altura = QLineEdit()
+
+        self.lbl_largura = QLabel('Largura')
+        self.txt_largura = QLineEdit()
+
+        self.lbl_profundidade = QLabel('Prufundidade')
+        self.txt_profundidade = QLineEdit()
+
+        self.lbl_resultade = QLabel()
+        self.btn_calcular = QPushButton('Calcular')
+
+        layout = QVBoxLayout()
+
 
 app = QApplication(sys.argv)
 principal = MainWindow()
